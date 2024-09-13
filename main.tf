36c5dca348baa45fb08147d2059e7a5f35dc8bec
unknown <krishanb281988@gmail.com>
2024-09-13 23:11:01 +0530

    tt

--- main.tf
+++ main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source    = "https://github.com/Krishan7567/tf-module1-vpc.git"
+  source    = "git::https://github.com/Krishan7567/tf-module1-vpc.git"
   VPC_CIDR  =  var.VPC_CIDR
 }
 
